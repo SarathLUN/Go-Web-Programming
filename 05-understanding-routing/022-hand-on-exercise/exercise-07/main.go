95a618500007748ca10c030a18960093f3288129
TONY <tony@TONYs-MacBook-Pro.local>
2021-03-28 23:53:40 +0700

    05-understanding-routing/022-hand-on-exercise/exercise-07

---
+++ 05-understanding-routing/022-hand-on-exercise/exercise-07/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer lis.Close()
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		serve(conn)
+	}
+}
+
+func serve(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if ln == "" {
+			// when ln is empty, header is done
+			fmt.Println("THIS IS THE END OF THE HTTP REQUEST HEADERS")
+			break
+		}
+	}
+	body := "CHECK OUT THE RESPONSE BODY PAYLOAD"
+	io.WriteString(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/plain\r\n")
+	io.WriteString(conn, "\r\n")
+	io.WriteString(conn, body)
+}
