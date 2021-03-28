384ad917d061ddcf31b7575e5f0f54419a08a6d9
TONY <tony@TONYs-MacBook-Pro.local>
2021-03-29 00:57:21 +0700

    05-understanding-routing/022-hand-on-exercise/exercise-08

---
+++ 05-understanding-routing/022-hand-on-exercise/exercise-08/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer lis.Close()
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		serve(conn)
+	}
+}
+
+func serve(conn net.Conn) {
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+	var i int
+	var rMethod, rURI string
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		if i == 0 {
+			// we're in REQUEST LINE
+			xs := strings.Fields(ln)
+			rMethod = xs[0]
+			rURI = xs[1]
+			fmt.Println("Method:", rMethod)
+			fmt.Println("URI:", rURI)
+		}
+		if ln == "" {
+			// when ln is empty, header is done
+			fmt.Println("THIS IS THE END OF THE HTTP REQUEST HEADERS")
+			break
+		}
+		i++
+	}
+	body := "CHECK OUT THE RESPONSE BODY PAYLOAD"
+	body += "\n"
+	body += rMethod
+	body += "\n"
+	body += rURI
+	io.WriteString(conn, "HTTP/1.1 200 OK\r\n")
+	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
+	fmt.Fprint(conn, "Content-Type: text/plain\r\n")
+	io.WriteString(conn, "\r\n")
+	io.WriteString(conn, body)
+}
