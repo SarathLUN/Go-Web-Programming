b08a2f5a9184c5836c5e29700825eea29290260a
TONY <tony@TONYs-MacBook-Pro.local>
2021-04-14 20:36:23 +0700

    08-creating-state/02-redirect/01-303-SeeOther

---
+++ 08-creating-state/02-redirect/01-303-SeeOther/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
+	http.HandleFunc("/barred", barred)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Your request method at foo:", r.Method)
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Your request method at bar:", r.Method)
+	http.Redirect(w, r, "/barred", http.StatusSeeOther)
+}
+
+func barred(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Your request method at barred:", r.Method)
+}
