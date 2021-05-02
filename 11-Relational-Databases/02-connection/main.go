58e31cb25c2af1a21e4b5b42d0b94299b1d92dd5
TONY <tony@TONYs-MacBook-Pro.local>
2021-05-02 21:34:21 +0700

    - 11-Relational-Databases/02-connection

---
+++ 11-Relational-Databases/02-connection/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"io"
+	"log"
+	"net/http"
+)
+
+var db *sql.DB
+var err error
+
+func main() {
+	db, err = sql.Open("mysql", "admin:yzTvwnqe6AJ6CYb@tcp(database-1.ch3fcoesrgva.us-east-2.rds.amazonaws.com:3306)/test2?charset=utf8")
+	check(err)
+	defer db.Close()
+
+	err = db.Ping()
+	check(err)
+
+	http.HandleFunc("/", index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	err := http.ListenAndServe(":8080", nil)
+	check(err)
+
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	_, err := io.WriteString(w, "Successful")
+	check(err)
+}
+
+func check(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/SarathLUN/Go-Web-Programming
 go 1.16
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0
 	github.com/julienschmidt/httprouter v1.3.0
-	github.com/satori/go.uuid v1.2.0 // indirect
-	golang.org/x/crypto v0.0.0-20210415154028-4f45737414dc // indirect
+	github.com/satori/go.uuid v1.2.0
+	golang.org/x/crypto v0.0.0-20210415154028-4f45737414dc
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
