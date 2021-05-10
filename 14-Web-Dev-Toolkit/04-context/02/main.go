ae720ae88e9639fcc42afc66569afdebb493144b
TONY <tony@TONYs-MacBook-Pro.local>
2021-05-10 21:52:46 +0700

    - 14-Web-Dev-Toolkit/04-context

---
+++ 14-Web-Dev-Toolkit/04-context/02/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	log.Println(ctx)
+	fmt.Fprintln(w, ctx)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+	ctx = context.WithValue(ctx, "userID", 777)
+	ctx = context.WithValue(ctx, "fname", "Bond")
+	res := dbAccess(ctx)
+	fmt.Fprintln(w, res)
+}
+
+func dbAccess(ctx context.Context) int {
+	uid := ctx.Value("userID").(int)
+	return uid
+}
